dbaecb81ad84ccf6523a6a8258ef66af140b9b78
João Carlos K. Dioscanio <85809525+JoaoDioscanio@users.noreply.github.com>
2026-05-13 22:33:40 -0300

    Add files via upload

---
+++ Aulas/Aula 14/script.js
@@ -0,0 +1,13 @@
+var x = 5;
+    if (x > 5) {
+        console.log("é maior que 5");
+    } else {
+        console.log("é menor ou igual a 5");
+    }
+for (var i = 0; i <= 100; i = i + 2){
+    console.log(i);
+}
+while (x<=100){
+    console.log(x);
+    x = x + 2;
+}
