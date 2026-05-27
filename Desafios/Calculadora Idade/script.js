0305e8b1e5d0104d4e4ca7ba11aa0b94bb5d2be3
João Carlos K. Dioscanio <85809525+JoaoDioscanio@users.noreply.github.com>
2026-05-26 22:22:14 -0300

    Update script.js

--- Desafios/Calculadora Idade/script.js
+++ Desafios/Calculadora Idade/script.js
@@ -1,14 +1,16 @@
-document.getElementById("meu-elemento").innerHTML = "<h1>Aprendendo JavaScript com o melhor professor da UVV!!!</h1>";
+//document.getElementById("meu-elemento").innerHTML = "<h1>Aprendendo JavaScript com o melhor professor da UVV!!!</h1>";
 
 
 function pegarvalor() {
-    // Digamos que o valor recebido seja "20-05-2000"
-    var inputDate = document.getElementById("data_nascimento");
-    var valorDataRaw = inputDate.value;
+    var dataN = document.getElementById("data_nascimento").value;
+    var hoje = new Date().getFullYear();
 
-    if (!valorDataRaw) {
-        window.alert("Por favor, digite uma data!");
+    if (dataN == 0)
+    {
+        alert("Digite um valor válido!");
         return;
-
     }
+    var idade = 0;
+    var idade = hoje - dataN;
+    document.getElementById("idade").innerHTML = "Você tem "+idade+" anos";
 }
